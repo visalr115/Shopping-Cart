391fc59969f1799eaf8f34039073d33cea0472c1
visalyadav115 <visal.yadav@redlooptech.com>
2017-11-27 05:30:17 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
       <img className="logo" src="/image/main.png" alt="" />
+      <input className="searchbar" type="text" placeholder="Search for natural, sustainable and handmade products" />
+      <button className="green" type="submit"><i className="fa fa-search"></i></button>
+      <i className="fa fa-user-circle-o" />
+      <i className="fa fa-shopping-cart" />
         </header>
       </div>
     );
