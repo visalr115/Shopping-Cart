a5520cba9ded0b39e6e3ef7ef9ed6f0fe9111dcd
visalyadav115 <visal.yadav@redlooptech.com>
2017-11-27 15:04:47 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -6,12 +6,78 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-      <img className="logo" src="/image/main.png" alt="" />
-      <input className="searchbar" type="text" placeholder="Search for natural, sustainable and handmade products" />
-      <button className="green" type="submit"><i className="fa fa-search"></i></button>
-      <i className="fa fa-user-circle-o" />
-      <i className="fa fa-shopping-cart" />
+          <img className="logo" src="/image/main.png" alt="" />
+          <input className="searchbar" type="text" placeholder="Search for natural, sustainable and handmade products" />
+          <button className="green" type="submit"><i className="fa fa-search"></i></button>
+          <i className="fa fa-user-circle-o" />
+          <i className="fa fa-shopping-cart" />
         </header>
+        <div className="box">
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+          <div className="small">
+            <div className="proimg">
+              <img src="https://www.coghlans.com/images/products/products-camp-kitchen-thumb.jpg" alt="" />
+            </div>
+            <h3>Product Name</h3>
+            <h2>Price</h2>
+            <button className="buynow">Add To Cart</button>
+          </div>
+        </div>
       </div>
     );
   }
